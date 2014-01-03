e9a0f80d7576a349ce73bed77323d7061ce243a9
Joe Sutton <hungryed6@gmail.com>
2014-01-03 16:50:21 -0500

    added building creation spec

--- spec/features/user_creates_a_building_spec.rb
+++ spec/features/user_creates_a_building_spec.rb
@@ -4,7 +4,15 @@ feature 'user creates a building spec' do
   scenario 'user submits valid information' do
     visit root_path
     click_link 'Create New Building'
-    fill_in ''
+    fill_in 'Address', with: '200 Moon St.'
+    fill_in 'City', with: 'Crater'
+    select 'MA', from: 'State'
+    fill_in 'Postal Code', with: '27106'
+    fill_in 'Description', with: 'Brick'
+    click_on 'Create Building'
+
+    expect(page).to have_content 'Create New Building'
+    expect(page).to have_content 'Building created successfully'
   end
 
   scenario 'user submits invalid information'
